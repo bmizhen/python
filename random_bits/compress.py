155468a90e467d763fe58203ef0675019f99ff39
Boris Mizhen <boris.mizhen@exosfinancial.com>
2020-10-01 21:18:57 -0400

    cleanup

---
+++ random_bits/compress.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+def multymap(pairs):
+    result = defaultdict(list)
+    for k, v in pairs:
+        result[k].append(v)
+    return result
+
+
+def compress(query):
+    """
+    input: “foo=1&foo=2&foo=3&blah=a&blah=b”
+    output: “foo=1,2,3&blah=a,b”
+    """
+
+    mmap = multymap2(term.split('=') for term in query.split('&'))
+    return '&'.join(f'{k}={",".join(v)}' for k, v in mmap.items())
+
+
+print(compress('foo=1&foo=2&foo=3&blah=a&blah=b'))
